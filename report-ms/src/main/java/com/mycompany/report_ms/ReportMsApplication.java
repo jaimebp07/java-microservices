4daa9d53855f6b3315391dfa91c76135a422838e
jaimebp07 <andrescodigo07@gmail.com>
2025-08-03 21:53:45 -0500

    🔧 files configurations

---
+++ report-ms/src/main/java/com/mycompany/report_ms/ReportMsApplication.java
@@ -0,0 +1,13 @@
+package com.mycompany.report_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportMsApplication.class, args);
+	}
+
+}
